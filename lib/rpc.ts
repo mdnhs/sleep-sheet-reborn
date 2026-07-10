f27bc2012b63e145751b5953b2b4c1643e331a6e
mdnhs <mdnhs.cse@gmail.com>
2026-07-10 23:49:22 +0600

    feat: add CORS headers to API routes in next.config.ts

--- lib/rpc.ts
+++ lib/rpc.ts
@@ -2,5 +2,8 @@
 import { AppType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
 
-// export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
+export const client = hc<AppType>(
+  typeof window !== "undefined" 
+    ? window.location.origin 
+    : process.env.NEXT_PUBLIC_APP_URL!
+);
--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,19 @@ const nextConfig: NextConfig = {
       { source: "/facebook-feed.json", destination: "/facebook-feed?format=json" },
     ]
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
